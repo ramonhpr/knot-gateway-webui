b88fe496d4aa27de21f06cf1c90ca5be39deb7eb
Ramon Henrique <rhpr@cin.ufpe.br>
2017-08-30 19:17:15 -0300

    Validate input in devices route

---
+++ app/schemas/devices.js
@@ -0,0 +1,27 @@
+var joi = require('joi');
+var REGEX_MAC = /^([0-9A-Fa-f]{2}[:-]){7}([0-9A-Fa-f]{2})$/;
+
+var upsert = {
+  name: joi
+    .string()
+    .required(),
+  mac: joi
+    .string()
+    .regex(REGEX_MAC)
+    .required(),
+  last_beacon: joi
+    .number()
+    .required()
+};
+
+var remove = {
+  id: joi
+    .string()
+    .regex(REGEX_MAC)
+    .required()
+};
+
+module.exports = {
+  upsert: upsert,
+  remove: remove
+};
